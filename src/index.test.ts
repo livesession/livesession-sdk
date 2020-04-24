afa65ec168f436b1984cf078856642211968ad6b
Marcel <marcel.bednarz16@gmail.com>
2020-04-24 17:32:16 +0200

    add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,24 +1,37 @@
 import livesession from "./index";
 import api from "./api";
 
-test("Call any other function before init", () => {
-  expect(() => {
-    livesession.setOptions({ rootHostname: "test" });
-    throw new Error();
-  }).toThrow();
-});
-
-it("should call init method", () => {
-  const initScript = jest.fn((trackID: string, options?: object) => {
-    livesession.init("test");
-    return api.init(trackID, options);
+describe("calling functions", () => {
+  test("Call any other function before init", () => {
+    expect(() => {
+      livesession.setOptions({ rootHostname: "test" });
+    }).toThrow();
   });
-  initScript("jkjfdsfds");
-  expect(initScript).toBeCalledTimes(1);
 });
 
-test("Check if script is added", () => {
-  const initScript = jest.fn(() => window.__ls && console.warn("LiveSession already inited (skipping init() call)"));
-  initScript();
-  expect(initScript).toHaveReturned();
+describe("Adding script", () => {
+  const warning = "LiveSession already inited (skipping init() call)";
+
+  const originalWarn = console.warn;
+  afterEach(() => (console.warn = originalWarn));
+
+  describe("Check adding script and console warn", () => {
+    let consoleOutput = [];
+    const mockedWarn = (output: string) => consoleOutput.push(output);
+    beforeEach(() => (console.warn = mockedWarn));
+
+    it("should call init method", () => {
+      const initScript = jest.fn((trackID: string, options?: object) => {
+        livesession.init(trackID);
+        return api.init(trackID, options);
+      });
+      initScript("jkjfdsfds");
+      expect(initScript).toBeCalledTimes(1);
+      expect(consoleOutput).toEqual([]);
+    });
+    it("Render script more than once", () => {
+      livesession.init("test2");
+      expect(consoleOutput).toEqual([warning]);
+    });
+  });
 });
